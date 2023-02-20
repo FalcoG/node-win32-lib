0cab7aec4bdd925c01a7ba9eaf385eaeed67a17c
Falco Gerritsjans <alias.fcb319@falco.pro>
2023-02-20 14:57:48 +0100

    fix(shell32): expand SHAppBarMessage tests

--- tests/shell32/SHAppBarMessage.test.js
+++ tests/shell32/SHAppBarMessage.test.js
@@ -1,12 +1,24 @@
-import { NativeStruct, NativeStructBasic } from '../../lib/native-types.js'
+import { NativeStruct } from '../../lib/native-types.js'
 import { shell32 } from '../../lib/native-libraries.js'
 import NativeConstants from '../../lib/native-constants.js'
+
+test('get taskbar state', () => {
+  const appbarData = new NativeStruct.APPBARDATA()
+  appbarData.cbSize = NativeStruct.APPBARDATA.size
+
+  const state = shell32.SHAppBarMessage(NativeConstants.ABM_GETSTATE, appbarData.ref())
+
+  // 0 = default state
+  // 1 = ABS_AUTOHIDE = autohide enabled
+
+  expect(state).toBe(0)
+})
 
 test('get taskbar position', () => {
   const appbarData = new NativeStruct.APPBARDATA()
   const result = shell32.SHAppBarMessage(NativeConstants.ABM_GETTASKBARPOS, appbarData.ref())
 
-  expect(result).toBe(true)
+  expect(result).toBe(1)
 
   const comparator = 
     appbarData.rc.left || appbarData.rc.top || appbarData.rc.right || appbarData.rc.bottom
