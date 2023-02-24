3519373a59a829d69b3aeb8498842d89a0984dc3
Falco Gerritsjans <alias.fcb319@falco.pro>
2023-02-24 21:39:16 +0100

    feat(user32): create FindWindowW, FindWindowExW and GetWindowTextW

---
+++ tests/user32/_window.test.js
@@ -0,0 +1,61 @@
+import { spawn } from 'child_process'
+import { user32 } from '../../lib/native-libraries.js'
+
+
+describe('Window testing', () => {
+  let spawnedApplication, spawnedApplicationHandle
+
+  /**
+   * Check if the application is ready before doing other functions
+   */
+  beforeAll(async () => {
+    let attempts = 0, loop
+
+    spawnedApplication = spawn('C:\\Program Files\\Windows NT\\Accessories\\wordpad.exe')    
+    spawnedApplicationHandle = await new Promise((resolve, reject) => {
+      loop = setInterval(() => {
+        const handle = user32.FindWindowExW(0, 0, null, Buffer.from('Document - WordPad\0', 'ucs2'))
+        
+        if (handle !== 0) resolve(handle)
+        else if (attempts > 5) reject(handle)
+  
+        attempts++
+      }, 100)
+    })
+  
+    clearInterval(loop)
+
+    return !!spawnedApplicationHandle
+  })
+
+  test('FindWindowExW', () => {
+    const hWnd = user32.FindWindowExW(0, 0, null, Buffer.from('Document - WordPad\0', 'ucs2'))
+    expect(hWnd).toBe(spawnedApplicationHandle)
+  })
+
+  test('FindWindowW', () => {
+    const hWnd = user32.FindWindowW(null, Buffer.from('Document - WordPad\0', 'ucs2'))
+    expect(hWnd).toBe(spawnedApplicationHandle)
+  })
+
+  test('GetWindowTextW', () => {
+    const expectedText = 'Document - WordPad'
+    const maxCharacters = 100
+
+    // prepare a buffer for the native function call
+    const bufferText = Buffer.alloc(maxCharacters * 2)
+    const windowTextLength = user32.GetWindowTextW(spawnedApplicationHandle, bufferText, maxCharacters)
+
+    // set up a buffer for comparison purposes
+    const expectedBuffer = Buffer.alloc(maxCharacters * 2)
+    expectedBuffer.write(expectedText, 'ucs2')
+
+    expect(windowTextLength).toBe(expectedText.length)
+    expect(Buffer.compare(bufferText, expectedBuffer)).toBe(0)
+  })
+  
+  afterAll(() => {
+    spawnedApplication.kill('SIGINT')
+  })
+})
+
