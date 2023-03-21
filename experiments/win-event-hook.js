a0db57f5fbdec22420dac0c467b04b204c7ff8fb
Falco Gerritsjans <alias.fcb319@falco.pro>
2023-03-21 23:26:28 +0100

    feat(experiments): automatically retrieve the application name for the foreground window

--- experiments/win-event-hook.js
+++ experiments/win-event-hook.js
@@ -1,8 +1,8 @@
 import ref from 'ref-napi'
 import ffi from 'ffi-napi'
-import { user32, kernel32, psapi, oleacc } from '../lib/native-libraries.js'
+import { user32, kernel32, psapi, oleacc, apiMsWinCore } from '../lib/native-libraries.js'
 import NativeConstants from '../lib/native-constants.js'
-import { NativeTypes } from '../lib/native-types.js'
+import { NativeStructBasic, NativeTypes } from '../lib/native-types.js'
 
 const getMessage = () => test32.GetMessageA(ref.alloc(msgPtr), null, 0, 0)
 
@@ -28,7 +28,7 @@ const WinEventProc = ffi.Callback(
     const processId = ref.alloc(NativeTypes.LPDWORD)
     const threadId = user32.GetWindowThreadProcessId(hwnd, processId)
     const PID = processId.readInt32LE(0)
-    console.log('hwnd', hwnd, 'threadid', threadId, 'lpdwProcessId', PID)
+    console.log('\nhwnd', hwnd, 'threadid', threadId, 'lpdwProcessId', PID)
 
     const processHandle = kernel32.OpenProcess(
       NativeConstants.PROCESS_QUERY_INFORMATION | NativeConstants.PROCESS_VM_READ,
@@ -36,17 +36,74 @@ const WinEventProc = ffi.Callback(
       PID
     )
 
-    const moduleFileNameLength = 100
-    const moduleFileName = Buffer.alloc(moduleFileNameLength * 2)
-
+    const moduleFileNameLength = 260 // MAX_PATH is long gone but let's do it this way for now
+    const moduleFileName = Buffer.alloc(moduleFileNameLength * 2) 
     const moduleFile = psapi.GetModuleFileNameExW(processHandle, null, moduleFileName, moduleFileNameLength)
+    const moduleFileNameSliced = moduleFileName.toString('ucs2').slice(0, moduleFile)
     
-    console.log('moduleFile', moduleFile, moduleFileName.toString('ucs2').slice(0, moduleFile))
-    console.log('open handle', processHandle)
+    console.log('foreground process path', moduleFile, moduleFileNameSliced)
+
+    const baseNameLength = 100
+    const baseName = Buffer.alloc(baseNameLength * 2)
+    const baseNameRealLength = psapi.GetModuleBaseNameW(processHandle, null, baseName, baseNameLength)
+
+    console.log('foreground process', baseName.toString('ucs2').slice(0, baseNameRealLength))
+
+    const fileVersionSize = apiMsWinCore.GetFileVersionInfoSizeW(moduleFileName, null)
+    const fileVersionBuffer = Buffer.alloc(fileVersionSize * 2)
+    const fileVersion = apiMsWinCore.GetFileVersionInfoW(
+      moduleFileName, null, fileVersionSize, fileVersionBuffer)
+
+    if (!fileVersion) {
+      console.log('insufficient permissions to read this file')
+      return
+    }
+
+    const pBlock = fileVersionBuffer
+    const lpSubBlock = Buffer.from('\\VarFileInfo\\Translation\0', 'ucs2')
+    const lpTranslate = ref.alloc(NativeTypes.LPVOID)
+    const puLen = ref.alloc(NativeTypes.PUINT)
+
+    const query = apiMsWinCore.VerQueryValueW(
+      pBlock,
+      lpSubBlock,
+      lpTranslate,
+      puLen
+    )
+
+    if (query) {
+      // todo: can we interpret this pointer as a struct? it should be 'LANGANDCODEPAGE'
+      const number = ref.readPointer(lpTranslate, 0, puLen.readUInt32LE(0));
+      const langId =  number.readUInt16LE(0x00)
+      const charset = number.readUInt16LE(0x02)
+
+      const langIdHex = ('0000' + (langId).toString(16)).slice(-4).toUpperCase()
+      const charsetHex = ('0000' + (charset).toString(16)).slice(-4).toUpperCase()
+
+      // https://learn.microsoft.com/en-us/windows/win32/menurc/varfileinfo-block
+      console.log('langId', langId, 'langid (hex)', langIdHex, 'charset', charset)
+
+      // fd = file description
+      const fd_lpSubBlock = Buffer.from(`\\StringFileInfo\\${langIdHex}${charsetHex}\\FileDescription\0`, 'ucs2')
+      const fd_lpBuffer = ref.alloc(NativeTypes.LPVOID)
+      const fd_puLen = ref.alloc(NativeTypes.PUINT)
+      const queryName = apiMsWinCore.VerQueryValueW(
+        pBlock,
+        fd_lpSubBlock,
+        fd_lpBuffer,
+        fd_puLen
+      )
+
+      if (queryName) {
+        const output = ref.readPointer(fd_lpBuffer, 0, fd_puLen.readUInt32LE() * 2)
+        console.log(`current application: ${output.toString('ucs2')} (len: ${fd_puLen.readUint32LE()})`)
+      } else {
+        console.log('unable to determine application name')
+      }
+    }
 
     if (processHandle) {
-      const close = kernel32.CloseHandle(processHandle)
-      console.log('close handle', close)
+      kernel32.CloseHandle(processHandle)
     }
   }
 )
